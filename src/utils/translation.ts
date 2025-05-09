c67e185a00e289d06a1dc6b2784581c866e75cfe
Carlos Murillo <craftedbycamo@gmail.com>
2025-05-08 22:19:22 -0700

    actualizacion

---
+++ src/utils/translation.ts
@@ -0,0 +1,22 @@
+// src/utils/translations.ts
+
+export const translations = {
+  es: {
+    services: { label: 'Servicios', path: 'services' },
+    aboutUs: { label: 'Sobre Nosotros', path: 'about-us' },
+    portfolio: { label: 'Portafolio', path: 'portfolio' },
+    gallery: { label: 'Galería', path: 'gallery' },
+    blog: { label: 'Blog', path: 'blog' },
+    testimonies: { label: 'Testimonios', path: 'testimonies' },
+    contact: { label: 'Contacto', path: 'contact' },
+  },
+  en: {
+    services: { label: 'Services', path: 'services' },
+    aboutUs: { label: 'About Us', path: 'about-us' },
+    portfolio: { label: 'Portfolio', path: 'portfolio' },
+    gallery: { label: 'Gallery', path: 'gallery' },
+    blog: { label: 'Blog', path: 'blog' },
+    testimonies: { label: 'Testimonies', path: 'testimonies' },
+    contact: { label: 'Contact', path: 'contact' },
+  },
+};
